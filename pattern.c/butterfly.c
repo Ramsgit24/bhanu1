67bb9fbce52574086c68b216defdadf4481dcc95
Bhanu <bhanuprakashkota0.com>
2024-11-03 10:52:26 +0530

    adding pattern files

---
+++ pattern.c/butterfly.c
@@ -0,0 +1,30 @@
+//#include<stdio.h>
+//int main(){
+//	int i,j,n,space;
+//	printf("Enter size :");
+//	scanf_s("%d",&n);
+//	for(i=1;i<=n;i++){
+//		for(j=1;j<=i;j++){
+//			printf("*");
+//		}
+//		for(space =1;space<=2*(n-i);space++){
+//			printf(" ");
+//		}
+//		for(j=1;j<=i;j++){
+//			printf("*");
+//		}
+//		printf("\n");
+//	}
+//	for(i=n-1;i>=1;i--){
+//		for(j=1;j<=i;j++){
+//			printf("*");
+//		}
+//		for(space=1;space<=2*(n-i);space++){
+//			printf(" ");
+//		}
+//		for(j=1;j<=i;j++){
+//			printf("*");
+//		}
+//		printf("\n");
+//	}
+//}
